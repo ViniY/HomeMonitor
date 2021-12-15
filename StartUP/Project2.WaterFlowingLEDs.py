5f938bd252e3e950bc1a66c8ab9dbc3d3693513b
ViniY <38173292+ViniY@users.noreply.github.com>
2021-12-16 10:27:56 +1300

    Update Project2.WaterFlowingLEDs.py

--- StartUP/Project2.WaterFlowingLEDs.py
+++ StartUP/Project2.WaterFlowingLEDs.py
@@ -1,14 +1,18 @@
 from machine import Pin
 from time import sleep
 
-
 Led_red = Pin(0, Pin.OUT)
 Led_green = Pin(1, Pin.OUT)
 Led_yewllow = Pin(2, Pin.OUT)
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)
 
-time_interval = 0.2
+time_interval = 2
 
 while True:
+  if button.value():
+    time_interval = time_interval / 1.414
+  if time_interval < 0.1:
+    time_interval = 2 
   Led_red.toggle()
   sleep(interval)
   
