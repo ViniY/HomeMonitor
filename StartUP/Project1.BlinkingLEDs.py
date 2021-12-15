08f0c6b51561886cc674ea89d50b28b84b86af9d
ViniY <38173292+ViniY@users.noreply.github.com>
2021-12-16 10:19:25 +1300

    Update and rename main.py to Project1.BlinkingLEDs.py
    Update the pin layout.

--- StartUP/Project1.BlinkingLEDs.py
+++ StartUP/Project1.BlinkingLEDs.py
@@ -2,9 +2,9 @@ from machine import Pin
 from machine import Pin, Timer
 import utime
 
-led_red = Pin(15, Pin.OUT)
-led_yellow = Pin(14, Pin.OUT)
-#led_green = Pin(13, Pin.OUT)
+led_red = Pin(0, Pin.OUT)
+led_yellow = Pin(1, Pin.OUT)
+led_green = Pin(2, Pin.OUT)
 timer = Timer()
 counter = 0
 def blink(timer):
@@ -17,6 +17,5 @@ def blink(timer):
     if counter%3 ==0:
         led_yellow.toggle()
     if counter%5 ==0:
-        print("gre")
-        #led_green.toggle()
+        led_green.toggle()
 timer.init(freq =2.5, mode = Timer.PERIODIC, callback =blink)
