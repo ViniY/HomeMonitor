d2018e282a4244b7580f7063f7f86e0548ea35d6
ViniY <vincentyu9603@gmail.com>
2021-12-18 14:38:31 +1300

    Update Project2.WaterFlowingLEDs.py

--- StartUP/Project2.WaterFlowingLEDs.py
+++ StartUP/Project2.WaterFlowingLEDs.py
@@ -6,11 +6,11 @@ Led_green = Pin(1, Pin.OUT)
 Led_yewllow = Pin(2, Pin.OUT)
 button = Pin(14, Pin.IN, Pin.PULL_DOWN)
 
-time_interval = 2
+interval = 2
 
 while True:
   if button.value():
-    time_interval = time_interval / 1.414
+    interval = interval / 1.414
   if time_interval < 0.1:
     time_interval = 2 
   Led_red.toggle()
