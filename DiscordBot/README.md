0d131c60bb40eb133fc6542acb77dee037d62e4d
ViniY <vincentyu9603@gmail.com>
2022-01-18 11:02:07 +1300

    a Simple DiscordBot

---
+++ DiscordBot/README.md
@@ -0,0 +1,23 @@
+# Jarvis at your service
+
+
+<h3> Smartest Jarvis : </h3>
+This is part of the project, everything we wanna do we have to press the button and look at the screens to get informed what is going on. <br />
+An new idea comes into my head when I go over Iron man AGAIN. <br />
+Why not have a Jarvis baby at home :D.<br />
+So here we are in this mini-project.<br />
+
+## Objective & or we say what I want it to do
+Ideally, it should be able to understand what you said, not only just recognize the word by word, but meanings, which I believe many people are trying to do the same thing in speech recognition, and they did a brilliant job. So in my prototype I will just keep it simple and use the developed packages. <br />
+
+
+## Current Functionality ：
+- Weather Report ：
+    - The mini Jarvis can fetch the weather information and read it out loud and clear
+- Location Report:
+    - Jarvis is capable of reporting the latitude and longtitude of the current location.
+- Voice recognition
+    - By the help of the packages, Jarvis is capable of recognizing the words we said and do work with its capable functionalities.
+
+## Later goal
+For sure it needs more functionalities, as I am developing the homemonitor, the programme get more inputs from surrounding like the tempreture, gas, light and so on, which means Jarvis can be able to know its surrounding environments better and tells the user what we gonna facing right now. And later on, we can let Jarvis to control the magic switches of light, air-conditioner, and so on, and be our perfect home controller? I guess this gonna need a long time later, but I will squeeze the time and keep myself on it. Peace. 
---
+++ DiscordBot/main.py
@@ -0,0 +1,146 @@
+import os
+import discord
+import json
+import requests
+from replit import db
+from keep_alive import keep_alive
+from datetime import datetime
+from geotext import GeoText
+import random 
+from discord.ext import commands,tasks
+import os
+from dotenv import load_dotenv
+import youtube_dl
+
+client = discord.Client()
+# intents = discord.Intents().all()
+# client = discord.Client(intents=intents)
+# bot = commands.Bot(command_prefix='!',intents=intents)
+
+api_key="1ada8f64926d58a8a31c7d92ec230faa7c462347a14269d45ba31d471b6f6aa6"  
+# Enter the API key you got from the OpenWeatherMap website
+text_bobi = ["祖安大舞台 有妈你就来", "天工造物不测，怎么造出你这么个东西", "扔块肉在屏幕上，狗都比你会连招", "你脑子进水了吧，还是100°的那种沸水",
+        "我终于知道你是一个什么怪物了，原来你是臭水沟里老鼠蜕变的呀，难怪浑身臭气熏天呢！"
+        "别在卖萌嘟嘴剪到手了，都是被人叫做叔叔阿姨的人了", "其实我挺佩服你的，能长成这样，你长的比芙蓉姐还恶心，比凤姐还销魂。"]
+
+def time_now():
+  now = datetime.now()
+  return now
+
+
+def weatherToday(msg_in):
+  city_name = GeoText(msg_in).cities
+  city_name = city_name[0]
+
+  base_url = "http://api.openweathermap.org/data/2.5/weather?"
+  complete_url = base_url + "appid=" +'d850f7f52bf19300a9eb4b0aa6b80f0d' + "&q=" + city_name
+
+  response = requests.get(complete_url)
+  x = response.json()
+  if x["cod"] != "404":
+      y = x["main"]
+      current_temperature = y["temp"] - 273.15
+      current_temperature = round(current_temperature,2)
+      z = x["weather"]
+      wind = x["wind"]
+      windSpeed = wind['speed']
+
+      print(wind)
+      weather_description = z[0]["description"]
+
+      h = y["humidity"]
+  weather=[]
+  weather.append(weather_description)
+  weather.append(current_temperature)
+  weather.append(windSpeed)
+  weather.append(h)
+  return weather
+
+def addBobi(msg):
+  msg_ = msg.replace("$添加:", "")
+  text_bobi.append(msg)
+  return (msg_ + " 已添加")
+
+
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return quote
+def update_encouragements(encouraging_msg):
+  if "encouragements" in db.key():
+    encouragemetns = db["encouragemetns"]
+    encouragemetns.append(encouraging_msg)
+  else: 
+    db["encouragemetns"] = [encouraging_msg]  
+    # options = options + db["encouragements"]
+
+def delete_encouragment(index):
+  encouragements = db['encouragements']
+  if len(encouragements) > index:
+    del encouragements[index]
+    db["encouragements"] = encouragements
+
+def zanghuabobi(duixiang):
+  index = random.randint(0,len(text_bobi)-1)
+  words = text_bobi[index]
+  return words
+#=========================================Music=======================
+
+# @client.command(pass_context=True)
+# async def yt(ctx, url):
+#     author = ctx.message.author
+#     voice_channel = author.voice_channel
+#     vc = await client.join_voice_channel(voice_channel)
+
+#     player = await vc.create_ytdl_player(url)
+#     player.start()
+
+#===========================================================================
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return # if the msg from the bot then just return
+
+  if message.content == ('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+  
+  msg = message.content
+
+  if "time" in msg and "now" in msg:
+    timeNow = time_now()
+    await message.channel.send(timeNow)
+  
+  if "weather" or "Weather" in msg:
+    weatherReport = weatherToday(msg)
+    # await message.channel.send(weatherReport)
+
+    weather = weatherReport[0]
+    tempreture = weatherReport[1]
+    wind = weatherReport[2]
+    humidity = weatherReport[3]
+    await message.channel.send("The weather today is " + weather
+    + " the tempreture now is " + str(tempreture) + " and the wind is " + str(wind) + "m/s and the humidity is " + str(humidity))
+  if "喷他" in msg:
+    for i in range(5):
+      zanghua = zanghuabobi("")
+      await message.channel.send(zanghua)
+
+  if "$添加:" in msg:
+    returned = addBobi(msg)
+    await message.channel.send(returned)
+
+  # if "$play"
+
+
+my_secret = os.environ['dcrobotJarv1s']
+
+keep_alive()
+# bot.run(my_secret)
+client.run(my_secret)
