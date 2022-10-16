86e4fbbe311e8b71663c2ac0f0c53fa852f0a28f
ViniY <vincentyu9603@gmail.com>
2022-10-16 18:39:11 +1300

    remove .ds_store using git ignore

--- Jarvis/FaceRecognition/main.py
+++ Jarvis/FaceRecognition/main.py
@@ -1,6 +1,6 @@
 """Tempo Main
 Used For testing the face recognition part only
-    """
+    w"""
 from FaceRecognition.pplAtHome import PeopleAtHome
 # from FaceRecognition.faceRecognition import Face_detector
 if __name__ == '__main__':
